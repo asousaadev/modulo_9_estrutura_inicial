e256da982fc8d40a4138b5aba8c0161c69a06910
Assis Sousa <assissousa207@gmail.com>
2024-05-28 16:50:24 -0400

    add elemento a jquery

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
-    console.log(document.querySelector('header  button'));
-    console.log('#botao-cancelar');
-
-
-    // document.querySelector('header button').addEventListener('click', fuction(){
-
-    // })
-
     // call back simplificado
     $('header button').click(function () {
 
         alert("Expandir formulario");
+    })
+
+    $('form').on('submit', function (e) {
+        console.log("submit");
+        e.preventDefault();
     })
 
 })
