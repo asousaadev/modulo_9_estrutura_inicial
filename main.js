5263f0e3d76a3c5364d5fc57dcd5e07a5b500dea
Assis Sousa <assissousa207@gmail.com>
2024-05-30 11:36:17 -0400

    add image

--- main.js
+++ main.js
@@ -5,13 +5,26 @@ $(document).ready(function () {
         $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function(){
+    $('#botao-cancelar').click(function () {
         $('form').slideUp();
     })
 
     $('form').on('submit', function (e) {
-        console.log("submit");
         e.preventDefault();
+        const enderecoNovaImagem = $('#endereco-nova-imagem').val();
+        const novoItem = $('<li style ="Display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+            <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+            Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-nova-imagem').val('');
     })
 
 })
